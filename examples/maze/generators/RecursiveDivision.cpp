7d1e8d4c700a2720dc5316b2a3d6e0f0b2e0b344
Demetrius Nekos <dnekos2462@gmail.com>
2022-11-03 15:50:53 -0400

    started RecursiveDivision

---
+++ examples/maze/generators/RecursiveDivision.cpp
@@ -0,0 +1,45 @@
+#include "RecursiveDivision.h"
+#include "../World.h"
+#include "Random.h"
+#include <climits>
+
+// returns true if it should be Horizontal
+bool RecursiveDivision::ChooseOrientation(int width, int height)
+{
+    if (width < height)
+        return HORIZONTAL;
+    else if (height > width)
+        return VERTICAL;
+    else
+        return Random::Range(0, 1) == 0;
+}
+
+bool RecursiveDivision::Step(World* w) {
+  
+    return true;
+  }
+}
+void RecursiveDivision::Clear(World* world) {
+  visited.clear();
+  stack.clear();
+  auto sideOver2 = world->GetSize()/2;
+
+  for(int i=-sideOver2;i<=sideOver2;i++){
+    for(int j=-sideOver2;j<=sideOver2;j++){
+      visited[i][j] = false;
+    }
+  }
+}
+Point2D RecursiveDivision::randomStartPoint(World* world) {
+  auto sideOver2 = world->GetSize()/2;
+
+  for(int y=-sideOver2; y<=sideOver2; y++)
+    for(int x=-sideOver2; x<=sideOver2; x++)
+      if(!visited[y][x])
+        return {x,y};
+  return {INT_MAX, INT_MAX};
+}
+
+std::vector<Point2D> RecursiveDivision::getVisitables(World* w, const Point2D& p) {
+  
+}
---
+++ examples/maze/generators/RecursiveDivision.h
@@ -0,0 +1,28 @@
+#ifndef RECURSIVEBACKTRACKER_H
+#define RECURSIVEBACKTRACKER_H
+
+#include "../MazeGeneratorBase.h"
+#include "Point2D.h"
+#include <map>
+#include <vector>
+
+#define HORIZONTAL true;
+#define VERTICAL false;
+
+class RecursiveDivision: public MazeGeneratorBase {
+ private:
+  std::vector<Point2D> stack;
+  std::map<int, std::map<int, bool>> visited; // naive. not optimal
+
+  bool ChooseOrientation(int width, int height);
+  std::vector<Point2D> getVisitables(World* w, const Point2D& p);
+ public:
+  RecursiveBacktracker() = default;
+  std::string GetName() override {return "Recursive Back-Tracker";};
+  bool Step(World * world) override;
+  void Clear(World * world) override;
+};
+
+//struct 
+
+#endif  // RECURSIVEBACKTRACKER_H
