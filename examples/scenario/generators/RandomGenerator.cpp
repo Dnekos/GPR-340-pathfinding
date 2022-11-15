c66c1278baff78222bf162ef661538fb3728a399
Demetrius Nekos <dnekos2462@gmail.com>
2022-11-15 14:08:12 -0500

    starting thermal erosion

--- examples/scenario/generators/RandomGenerator.cpp
+++ examples/scenario/generators/RandomGenerator.cpp
@@ -14,7 +14,7 @@ std::vector<Color32> RandomScenarioGenerator::Generate(int sideSize, float displ
     {
         for (int c = 0; c < sideSize; c++) 
         {
-            float rgb = abs(noise.octave3D(c / 50.0, l / 50.0, displacement, 2) * 255);
+            float rgb = abs(noise.octave3D(c / 50.0, l / 50.0, displacement, 4) * 255);
             colors.emplace_back(rgb, rgb, rgb);
         //      double color = noise.noise(c/50.0,l/50.0);
         //      colors.emplace_back(color,color,color);
